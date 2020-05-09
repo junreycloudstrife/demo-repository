e29dea95491392e18319c663d200cff7e01eafaf
junreycloudstrife <junrey.cloudstrife@gmail.com>
2020-05-09 20:56:49 +0800

    Implemented event listeners for the buttons

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // This is a JavaScript code
 
-// var pageTitle = document.getElementById('page-title').innerHTML = "Hello World";
+document.getElementById('btn1').addEventListener('click',() => {
+    document.getElementById('page-title').innerHTML = "Hello World";
+})
+
+document.getElementById('btn2').addEventListener('click',() => {
+    document.getElementById('page-title').innerHTML = "Sample Title";
+})
+
+document.getElementById('btn3').addEventListener('click',() => {
+    document.getElementById('page-title').innerHTML = "Demo Repository";
+})
